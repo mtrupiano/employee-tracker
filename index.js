f3b4e0e4faaae1708d385c3566881194a1a92d1e
mtrupiano <markt6749@gmail.com>
2021-01-20 09:10:53 -0800

    drafting main loop

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const mysql     = require ('mysql');
 const inquirer  = require ('inquirer');
 const cTable    = require ('console.table');
 
+const queries = require ('./lib/employeeQuery.js');
+
+const mainPrompts = require('./json/mainPrompts.json');
+
 const connection = mysql.createConnection({
     host: '192.168.1.24',
     port: 3306,
@@ -14,3 +18,18 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
 });
+
+async function main() {
+
+    const select = await inquirer.prompt(mainPrompts);
+
+    switch (select) {
+        case "View all employees":
+            connection.query(queries.queryForAllEmployees(), function (err, result, fields) {
+                cTable(result);
+            });
+            break;
+        case "View employees by role":
+            break;
+    }
+}
