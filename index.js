68e51c0a0dc06ca78fbf9eb8c93dc6a997bc68e9
mtrupiano <markt6749@gmail.com>
2021-01-20 19:25:52 -0800

    Draft of addEmployee function

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const mysql     = require ('mysql');
+const mysql     = require ('mysql2');
 const inquirer  = require ('inquirer');
 const cTable    = require ('console.table');
 
 const queries = require ('./lib/employeeQuery.js');
 
 const mainPrompts = require('./json/mainPrompts.json');
-
 const connection = mysql.createConnection({
     host: '192.168.1.24',
     port: 3306,
@@ -14,10 +13,10 @@ const connection = mysql.createConnection({
     database: 'employees'
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-});
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('connected as id ' + connection.threadId);
+// });
 
 async function main() {
 
@@ -40,6 +39,7 @@ async function main() {
             break;
 
         case "Add employee":
+            await addEmployee();
             break;
         case "Remove employee":
             break;
@@ -59,6 +59,67 @@ async function main() {
             return;
     }
 
+}
+
+async function addEmployee() {
+
+    // Get list of possible roles from the database
+    const [roles, fields] = await connection.promise().query(queries.queryForAllRoles);
+
+    // Prompt user to input new employee's name and role
+    const newEmployee = await inquirer.prompt([
+        {
+            "type": "input",
+            "message": "Employee's first name? ",
+            "name": "first_name"
+        }, {
+            "type": "input",
+            "message": "Employee's last name? ",
+            "name": "last_name"
+        }, {
+            "type": "list",
+            "message": "Select the employee's role:",
+            "choices": roles.map((val) => {
+                return {
+                    "value": val.id,
+                    "name": val.Title
+                };
+            }),
+            "name": "role"
+        }
+    ]);
+
+    if (newEmployee.role === 1) { // New employee is a manager
+        newEmployee["manager"] = null;
+    } else {
+
+        // New employee is not a manager; query database for list of managers and
+        // present another prompt for user to select new employee's manager
+        const [managers, fields] = 
+            await connection.promise().query("SELECT * FROM employee WHERE role_id = 1");
+
+        const manager = await inquirer.prompt({
+            "type": "list",
+            "message": "Select the employee's manager:",
+            "name": "manager",
+            "choices": managers.map((val) => {
+                return {
+                    "value": val.id,
+                    "name": `${val.last_name}, ${val.first_name}`
+                }
+            })
+        });
+
+        newEmployee["manager"] = manager.manager;
+    }
+
+    // Insert new employee into database
+    await connection
+        .promise()
+        .query("INSERT INTO employee VALUES (DEFAULT, ?, ?, ?, ?)", 
+                Object.values(newEmployee));
+    
+    exiter();
 }
 
 function viewEmployeesByRole() {
