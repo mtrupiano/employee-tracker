5f90f0399f85f960fe6f1abe05998702697ffad6
mtrupiano <markt6749@gmail.com>
2021-01-20 21:07:56 -0800

    added managers to seed

--- sql/seed.sql
+++ sql/seed.sql
@@ -9,7 +9,9 @@ INSERT INTO role VALUES
 
 INSERT INTO employee VALUES 
     ( DEFAULT, "Joe",       "Rehfuss",  1, NULL ),
+    ( DEFAULT, "MF",       	"DOOM",  	1, NULL ),
+    ( DEFAULT, "Juice",     "WRLD",  	1, NULL ),
     ( DEFAULT, "Aslan",     "Ghodsian", 2, 1 ),
     ( DEFAULT, "Mark",      "Trupiano", 3, 1 ),
-    ( DEFAULT, "Daniel",    "Sasser",   3, 1 ),
-    ( DEFAULT, "Nolan",     "Stucky",   3, 1 );
+    ( DEFAULT, "Daniel",    "Sasser",   3, 2 ),
+    ( DEFAULT, "Nolan",     "Stucky",   3, 2 );
